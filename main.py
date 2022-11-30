54c51674b7a3f745384e52f8d847456dd8b5e67e
trizmera <mllebia@live.com>
2022-11-30 20:42:12 -0300

    "Final Completed Version"

--- main.py
+++ main.py
@@ -7,23 +7,33 @@
 # > Use symbols if the system allows
 # > Use a combination of letters and numbers, or a phrase like "many colors" using only the consonants, e.g., mnYcOlOrz
 import emoji
+import string
+import random
 
-print(emoji.emojize(':kissing_face_with_closed_eyes: Welcome to the Strong Password Generator :kissing_face_with_closed_eyes:'))
+print(emoji.emojize(':kissing_face_with_closed_eyes: Welcome to the Strong Password Generator'
+                    ' :kissing_face_with_closed_eyes:'))
 print("Select which type of password you are looking for: \n"
       " - A) if simple password with numbers and letters;\n"
       " - B) a password with upper and lowercase characters;\n"
       " - C) a super--mega--power password with everything included;")
 option = input(emoji.emojize(':writing_hand: | '))
 
+
+def simple_pass(size=8, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def complicated_pass(size=10, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def power_pass(size=12, chars=string.ascii_letters + string.digits + string.punctuation):
+      return ''.join(random.choice(chars) for _ in range(size))
+
 match option:
       case "A":
-      # simple_pass()
-          print(option)
+        print(emoji.emojize("Your new pass is: :locked: " + simple_pass()))
       case "B":
-      # complicated_pass()
-            print(option)
+        print(emoji.emojize("Your new pass is: :locked: " + complicated_pass()))
       case "C":
-      # power_pass()
-            print(option)
-      case default:
-            print(emoji.emojize(':warning: You did not chose a possible option! Look again :eyes:'))
+        print(emoji.emojize("Your new pass is: :locked: " + power_pass()))
+      case _:
+        print(emoji.emojize(':warning: You did not chose a possible option! Look again :eyes:'))
